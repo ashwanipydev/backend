283d7c40b3100c5f287eb0482aebf694b329920a
ashwani <pydev.ashwani@gmail.com>
2024-01-27 14:06:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express()
+
+const port = process.env.PORT|| 3000
+app.use(express.static('dist'))
+const jokes = [
+    {
+        id: 1,
+        title: "joke 1",
+        content: "this is joke"
+    },
+    {
+        id: 2,
+        title: "joke 2",
+        content: "this is joke"
+    }
+]
+
+app.get('/api/jokes', (req,res)=>{
+    res.send(jokes)
+})
+
+app.listen(port, ()=>{
+    console.log(`server ready ${port}`);
+})
